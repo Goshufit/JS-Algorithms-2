68c28a58a6fc5e4ebd78ccf2f402f0505c13ed3f
Jean-Pierre Graham <91097729+Goshufit@users.noreply.github.com>
2022-06-18 23:09:58 -0400

    Add segmenting of an array through iteration and push and slice method

--- script.js
+++ script.js
@@ -35,7 +35,26 @@ function longestWord(sen) {
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) { }
+function chunkArray(arr, len) {
+  //Initialize chunked array//
+  const chunkedArr = [];
+
+  //Set Index//
+  let i = 0;
+
+  //Loop while index is less than the array length//
+  while (i < arr.length) {
+    //Slice out from the index to the index + the chunk length and push on to the chunked array//
+    //The push() method adds one or more elements to the end of an array and returns the new length of the array.
+    //The slice() method returns a shallow copy of a portion of an array into a new array object selected from start to end (end not included) 
+    //where start and end represent the index of items in that array. The original array will not be modified. 
+    chunkedArr.push(arr.slice(i, i + len));
+    // Increment by chunk length
+    i += len;
+  }
+
+  return chunkedArr;
+}
 
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
